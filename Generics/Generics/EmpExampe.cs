2c59716c0a538a46895d26899fd8c89bce413fb7
Madhuezio <madhukorla555@gmail.com>
2022-02-07 17:54:35 +0530

    EmpEx

---
+++ Generics/Generics/EmpExampe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Employee1
+    {
+        string Name;
+        int Salary;
+        string EmployeeID;
+    }
+
+    class EmpExampe
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- Generics/Generics/ExampleEx.cs
+++ Generics/Generics/ExampleEx.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Generics
 {
     class ExampleEx
     {
-        public static void Main()
+        public static void Main8()
         {
-           
+            Console.Write("Enter input->");
+            string a = Console.ReadLine();
+            Regex re = new Regex("[0-9]{4}$");
+            Match match = re.Match(a);
+            if (match.Success)
+            {
+                Console.WriteLine("Success!!");
+            }
+            else
+            {
+                Console.WriteLine("Input valid string!!");
+            }
         }
     }
 }
