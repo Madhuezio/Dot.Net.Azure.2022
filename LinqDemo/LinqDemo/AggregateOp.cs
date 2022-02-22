bf841b3403d414804c755d3f09051c42b938acdf
Madhuezio <madhukorla555@gmail.com>
2022-02-22 17:39:51 +0530

    Linq-Operators

---
+++ LinqDemo/LinqDemo/AggregateOp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LinqDemo
+{
+    //aggregation operators
+
+    //aggregate
+    //average
+    //count
+    //longcount
+    //max
+    //min
+    //sum
+    class AggregateOp
+    {
+        static void Main8()
+        {
+            //Aggregate
+            IList<string> strList = new List<string>() { "one", "two", "three", "four" };
+            var commasepartedstring = strList.Aggregate((s1, s2) => s1 + " | " + s2);
+            Console.WriteLine(commasepartedstring);
+
+            //Average
+            IList<int> AvgList = new List<int>() { 10, 15, 35, 45, 75, 95 };
+            var avg = AvgList.Average();
+            Console.WriteLine("Average: {0}", avg);
+
+            //Count
+            IList<int> ConList = new List<int>() { 10, 21, 30, 45, 50 };
+            var totalElements = ConList.Count();
+            Console.WriteLine("Total Elements: {0}", totalElements);
+
+            //Max
+            IList<int> MaxList = new List<int>() { 10, 21, 45, 65, 75, 99 };
+            var largest = MaxList.Max();
+            Console.WriteLine("Largest Element: {0}", largest);
+
+            //Min
+            IList<int> MinList = new List<int>() { 5, 10, 20, 25, 75, 95 };
+            var smallest = MinList.Min();
+            Console.WriteLine("Smallest Element: {0}", smallest);
+
+            //Summ
+            IList<int> SumList = new List<int>() { 10, 25, 30, 45, 50, 99 };
+            var total = SumList.Sum();
+            Console.WriteLine("Sum: {0}", total);
+        }
+    }
+}
---
+++ LinqDemo/LinqDemo/Problem1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinqDemo
+{
+    public class Employee
+    {
+        public int id;
+        public string name;
+        public int dept_id;
+        public int add_id;
+    }
+    public class Department
+    {
+        public int dept_id;
+        public string dept_name;
+    }
+    class Problem1
+    {
+        static void Main(string[] args)
+        {
+
+           
+            List<Employee> employees = new List<Employee>()
+            {
+                 new Employee{ id = 123, name = "madhu babu",
+                      dept_id = 1},
+                 new Employee{ id = 456, name = "Edward",
+                      dept_id = 2},
+                 new Employee{ id = 789, name = "Ezio",
+                      dept_id = 1},
+                 new Employee{ id = 533, name = "Mihawk",
+                      dept_id = 4},
+            };
+
+            List<Department> departments = new List<Department>()
+             {
+                  new Department{ dept_id = 1, dept_name = "CSE" },
+                  new Department{ dept_id = 2, dept_name = "ECE" },
+                  new Department{ dept_id = 3, dept_name = "IT " },
+             };
+
+            var result = (from stu in employees
+
+                          join dept in departments on stu
+                          .dept_id equals dept
+                          .dept_id
+
+                          select new
+                          {
+                              ID = stu.id,
+                              Name = stu.name,
+                              DeptName = dept.dept_name,
+                          }).ToList();
+
+            foreach (var e in result)
+            {
+                Console.WriteLine("ID: " + e.ID +
+                            "--> Name: " + e.Name +
+                      "--> Department: " + e.DeptName);
+            }
+        }
+    }
+}
